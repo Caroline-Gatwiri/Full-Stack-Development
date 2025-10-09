37c6ffe5c08873aff00788b93dbfdf7de6b76a6a
Caroline Gatwiri <gatwiri123caroline@gmail.com>
2025-10-09 08:42:50 +0530

    Initial commit

---
+++ Website-JavaScript beginner/index.js
@@ -0,0 +1,6 @@
+// console.log(`Hello Bro`);
+
+// window.alert(`This is an alert`);
+
+document.getElementById("myH1").textContent = `Hello` ;
+document.getElementById("myP").textContent = `We can do this`
