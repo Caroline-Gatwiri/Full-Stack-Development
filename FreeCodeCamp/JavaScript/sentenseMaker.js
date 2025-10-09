f8b4c6732ecee1a28442573dd04c4ca49a407c98
Caroline Gatwiri <gatwiri123caroline@gmail.com>
2025-10-09 09:15:52 +0530

    Building a sentense maker

---
+++ FreeCodeCamp/JavaScript/sentenseMaker.js
@@ -0,0 +1,21 @@
+let adjective = "yellow";
+let noun = "car";
+let verb ="driving";
+let place = "Ahmedabad";
+let adjective2 ="green";
+let noun2 ="seat";
+
+let firstStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("First story: "+firstStory);
+
+adjective = "White";
+noun = "horse";
+verb = "running"
+place ="Mountains"
+adjective2 = "Black"
+noun2 = "Wolves"
+
+let secondStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+
+console.log("Second story: "+secondStory);
