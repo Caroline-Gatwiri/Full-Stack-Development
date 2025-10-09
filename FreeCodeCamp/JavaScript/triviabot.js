fc2c484878e974f3611c2326a6efea5a809fa63d
Caroline Gatwiri <gatwiri123caroline@gmail.com>
2025-10-09 09:01:17 +0530

    Trivia bot

---
+++ FreeCodeCamp/JavaScript/triviabot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Java Bot";
+let botLocation = "Java Island";
+let favoriteLanguage = "Java";
+
+console.log("My name is "+botName+" and I live on "+botLocation+".");
+console.log("My favorite programming language is "+favoriteLanguage+".");
+
+let codingFact = "A fun fact about"+favoriteLanguage+" is that it was initially named Oak, after an oak tree outside the office of its creator, James Gosling, but was later renamed Java after the trademark was found to be in use.";
+
+console.log(codingFact);
+
+codingFact = "The name Java derives from the island of "+favoriteLanguage+" in Indonesia, known as being the birth place of coffee";
+
+console.log(codingFact);
+
+codingFact = "So far this is a ll i know about "+favoriteLanguage+" .";
+
+console.log(codingFact);
+
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+"." )
